52fc8efbfa2adc616370f926ccccbd14829909f9
Luke <luke.y.prince@gmail.com>
2024-11-06 21:58:31 +0000

    untested basic wandb integration

--- train.py
+++ train.py
@@ -3,6 +3,7 @@
 #
 
 import torch
+import wandb
 
 # First import low_bits_training for MonkeyPatching TorchTitan.
 import low_bits_training  # noqa: F401
@@ -12,5 +13,7 @@ if __name__ == "__main__":
     print("TRAINING go brrrrr!")
     config = tt_train.JobConfig()
     config.parse_args()
+    wandb.init(project="low-bits-training", entity="graphcore", mode="online", group="test", config=config, sync_tensorboard=True)
     tt_train.main(config)
     torch.distributed.destroy_process_group()
+    wandb.finish()
