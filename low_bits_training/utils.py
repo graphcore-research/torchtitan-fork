fe2c907bd4e828fe391616a50ecbbae5c8fd48ef
Luke <luke.y.prince@gmail.com>
2024-11-07 15:14:50 +0000

    remove wandb init group

--- low_bits_training/utils.py
+++ low_bits_training/utils.py
@@ -1,24 +1,32 @@
 #
 # Copyright (c) 2024 Graphcore Ltd. All rights reserved.
 #
-from torchtitan.torchtitan.config_manager import JobConfig
 from typing import Dict
+
+from torchtitan.torchtitan.config_manager import JobConfig
+
 
 def job_config_to_config_dict(job_config: JobConfig) -> Dict[str, Dict[str, str]]:
     """
     JobConfig is created from Argument Parser as a two level object.
-    
+
     This a hacky method of converting JobConfig to a ConfigDict that can
     cleanly be consumed by wandb
-    
+
     """
-    first_level_args = [attr for attr in dir(job_config) if not (attr.startswith("_") or "parse" in attr)]
+    first_level_args = [
+        attr
+        for attr in dir(job_config)
+        if not (attr.startswith("_") or "parse" in attr)
+    ]
     config_dict = {}
     for arg1 in first_level_args:
         assert hasattr(job_config, arg1)
         config_dict[arg1] = {}
         second_level_config = getattr(job_config, arg1)
-        second_level_args = [attr for attr in dir(second_level_config) if not attr.startswith("_")]
+        second_level_args = [
+            attr for attr in dir(second_level_config) if not attr.startswith("_")
+        ]
         for arg2 in second_level_args:
             assert hasattr(second_level_config, arg2)
             config_dict[arg1][arg2] = getattr(second_level_config, arg2)
--- train.py
+++ train.py
@@ -3,11 +3,11 @@
 #
 
 import torch
-import low_bits_training.utils
-import wandb
 
 # First import low_bits_training for MonkeyPatching TorchTitan.
 import low_bits_training  # noqa: F401
+import low_bits_training.utils
+import wandb
 from torchtitan import train as tt_train
 
 if __name__ == "__main__":
@@ -15,7 +15,13 @@ if __name__ == "__main__":
     config = tt_train.JobConfig()
     config.parse_args()
     config_dict = low_bits_training.utils.job_config_to_config_dict(config)
-    wandb.init(project="low-bits-training", entity="graphcore", mode="online", group="test", config=config_dict, sync_tensorboard=True)
+    wandb.init(
+        project="low-bits-training",
+        entity="graphcore",
+        mode="online",
+        config=config_dict,
+        sync_tensorboard=True,
+    )
     tt_train.main(config)
     torch.distributed.destroy_process_group()
     wandb.finish()
