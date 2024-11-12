6e7d37fadd17944ba8e58fc03efa78f320595df5
Alexandre Payot <alexandrep@graphcore.ai>
2024-11-12 17:45:29 +0000

    Use a better condition for picking logging rank

--- low_bits_training/utils.py
+++ low_bits_training/utils.py
@@ -1,9 +1,12 @@
 #
 # Copyright (c) 2024 Graphcore Ltd. All rights reserved.
 #
+import os
 from typing import Dict
 
 from torchtitan.torchtitan.config_manager import JobConfig
+from torchtitan.torchtitan.metrics import _get_metrics_rank
+from torchtitan.torchtitan.parallelisms.parallel_dims import ParallelDims
 
 
 def job_config_to_config_dict(job_config: JobConfig) -> Dict[str, Dict[str, str]]:
@@ -31,3 +34,25 @@ def job_config_to_config_dict(job_config: JobConfig) -> Dict[str, Dict[str, str]
             assert hasattr(second_level_config, arg2)
             config_dict[arg1][arg2] = getattr(second_level_config, arg2)
     return config_dict
+
+
+def get_parallel_dims(job_config) -> ParallelDims:
+    world_size = int(os.environ["WORLD_SIZE"])
+    return ParallelDims(
+        dp_shard=job_config.training.data_parallel_shard_degree,
+        dp_replicate=job_config.training.data_parallel_replicate_degree,
+        tp=job_config.training.tensor_parallel_degree,
+        pp=job_config.experimental.pipeline_parallel_degree,
+        world_size=world_size,
+        enable_loss_parallel=job_config.training.enable_loss_parallel,
+    )
+
+
+def is_metrics_rank(config) -> bool:
+    """Calls torchtitan's `_get_metrics_rank` to figure out if we are on the
+    correct process to collect metrics
+
+    This check is needed for pipelining configurations.
+    """
+    metrics_rank = _get_metrics_rank(get_parallel_dims(config))
+    return int(os.environ["RANK"]) == metrics_rank
--- train.py
+++ train.py
@@ -4,11 +4,11 @@
 import os
 
 import torch
-import wandb
 
 # First import low_bits_training for MonkeyPatching TorchTitan.
 import low_bits_training  # noqa: F401
 import low_bits_training.utils
+import wandb
 from torchtitan import train as tt_train
 
 
@@ -17,20 +17,22 @@ def main():
     config = tt_train.JobConfig()
     config.parse_args()
     # Logging in W&B? TODO: custom config flag.
-    enable_wb_logging = config.metrics.enable_tensorboard
+    enable_wb_logging = config.metrics.enable_tensorboard and (
+        low_bits_training.utils.is_metrics_rank(config)
+        or not config.metrics.rank_0_only
+    )
     if enable_wb_logging:
         config_dict = low_bits_training.utils.job_config_to_config_dict(config)
 
         group_id = os.getenv("WANDB_GROUP_ID")
-        if int(os.environ['LOCAL_RANK']) == 0 or not config.metrics.rank_0_only:
-            wandb.init(
-                project="low-bits-training",
-                entity="graphcore",
-                mode="online",
-                config=config_dict,
-                sync_tensorboard=True,
-                group=group_id,
-            )
+        wandb.init(
+            project="low-bits-training",
+            entity="graphcore",
+            mode="online",
+            config=config_dict,
+            sync_tensorboard=True,
+            group=group_id,
+        )
     # Main TorchTitan training setup & loop
     tt_train.main(config)
     torch.distributed.destroy_process_group()
