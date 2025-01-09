92d34c13480044a2c110d116c8d76008281562c8
Paul Balanca <paulb@graphcore.ai>
2025-01-09 15:18:38 +0000

    Improve slurm setup for 64 GPUs cluster. (#76)
    * Improve slurm setup for 64 GPUs cluster.
    
    * `torchrun` nodes argument following slurm config;
    * Using `--ntasks-per-node` to avoid duplicating `--nodes` value;
    * Slurm job name following W&B naming;
    * Slurm log filename using job name, in `/data/checkpoints` per default;
    
    * Improvements on SBATCH_OUTPUT following AlexP review.

--- submit.sh
+++ submit.sh
@@ -13,4 +13,18 @@ if ! [ -z "${WANDB_PROJECT:-}" ]; then
   echo "W&B project URL: 'https://wandb.ai/graphcore/${WANDB_PROJECT}'"
 fi
 echo "W&B Name: '${WANDB_NAME}'"
-sbatch ${SBATCH_ARGS:-} multinode_trainer.slurm "$@"
+
+# Checkpointing & log directory + output name (with slurm job ID).
+if [ -z "${SBATCH_OUTPUT:-}" ]; then
+  export SBATCH_OUTPUT_DIR=${SBATCH_OUTPUT_DIR:-"/data/checkpoints"}
+  export SBATCH_OUTPUT="${SBATCH_OUTPUT_DIR}/${WANDB_NAME}/slurm-%j.out"
+fi
+
+# Standard naming for slurm job: project + W&B name.
+export SBATCH_JOB_NAME="low-bits-training-${WANDB_NAME}"
+
+SLURM_JOB_ID=$(sbatch ${SBATCH_ARGS:-} --parsable multinode_trainer.slurm "$@")
+echo "Submitted Slurm batch job ${SLURM_JOB_ID}"
+# Update sbatch output env. variable with proper slurm job id.
+# NOTE: useful for debugging to have the full & accurate log path in the console!
+echo "Slurm output logs: '${SBATCH_OUTPUT/"%j"/${SLURM_JOB_ID}}'"
